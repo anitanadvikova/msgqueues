f5890381b452d5108a7cedd2171d137eb75f755d
anitanadvikova <39883551+anitanadvikova@users.noreply.github.com>
2022-04-14 17:40:44 +0300

    Add files via upload

---
+++ src/main/java/com/study/crawler/configuration/RabbitMQConfiguration.java
@@ -0,0 +1,39 @@
+package com.study.crawler.configuration;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.Queue;
+
+@Configuration
+public class RabbitMQConfiguration {
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+
+    @Bean
+    public AmqpTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Queue receiveQueue() {
+        return new Queue("produce_queue");
+    }
+
+    @Bean
+    public Queue consumeQueue() {
+        return new Queue("consume_queue");
+    }
+
+
+}
